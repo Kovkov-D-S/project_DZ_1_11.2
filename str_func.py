0bce19c9116bc5439e1cf5c72d4b70766bc1f155
Kovkov-D-S <158774590+Kovkov-D-S@users.noreply.github.com>
2024-02-07 19:28:21 +0300

    fix docstrings functions

---
+++ str_func.py
@@ -0,0 +1,10 @@
+def make_capital_letters(string):
+    """Возращается все слова в �с �троке заглавными буквами"""
+    string_upper = string.upper()
+    return string_upper
+
+
+def make_title_letters(string2):
+    """Возвращает строку со словами с заглавной буквы"""
+    string_title = string2.title()
+    return string_title
