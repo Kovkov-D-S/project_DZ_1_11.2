52983bec8366f1bd6dcdc849f2a6315a5f6af588
Kovkov-D-S <158774590+Kovkov-D-S@users.noreply.github.com>
2024-02-07 19:13:45 +0300

    Revert "update main"

--- str_func.py
+++ str_func.py
@@ -1,10 +1,4 @@
 def make_capital_letters(string):
-    """Возращается все слова в строке заглавными буквами"""
+    """Возвращает строку со всеми заглавными буквами"""
     string_upper = string.upper()
     return string_upper
-
-
-def make_title_letters(string2):
-    """Возращает строку со слова с  заглавной буквы"""
-    string_title = string2.title()
-    return string_title
